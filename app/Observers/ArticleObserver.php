640cc4147f2ffa775b5b10ad31e4820f09ef1c44
Alain-Kay <kazadikayumba@gmail.com>
2022-12-29 23:06:34 +0100

    event listener for the slug

---
+++ app/Observers/ArticleObserver.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Article;
+use Illuminate\Support\Str;
+
+class ArticleObserver
+{
+    /**
+     * Handle the Article "created" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function created(Article $article)
+    {
+
+        $article->slug = Str::slug($article->title, '-');
+        $article->save();
+    }
+
+    /**
+     * Handle the Article "updated" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function updated(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the Article "deleted" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function deleted(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the Article "restored" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function restored(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the Article "force deleted" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function forceDeleted(Article $article)
+    {
+        //
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Article;
+use App\Observers\ArticleObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Article::observe(ArticleObserver::class);
+        
     }
 }
