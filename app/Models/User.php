7c2351bac85ba2375f899edfcbe49b500f2ea9e4
Alain-Kay <kazadikayumba@gmail.com>
2023-01-11 22:59:18 +0100

    Roles management

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    const USER_ROLE = 'USER';
+    const ADMIN_ROLE = 'ADMIN';
+
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +24,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
