ba3727b83b913afbad2c0fa4e316da43181d55f9
Alain-Kay <kazadikayumba@gmail.com>
2023-01-11 15:58:12 +0100

    Authentication

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,60 @@
+@extends('template')
+
+@section('content')
+<div class="container">
+    <h3 class="text-center my-5">Connexion</h3>
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Connexion') }}
+                                </button>
+                               
+                                    <a class="btn btn-link" href="{{ route('register') }}">
+                                        Pas encore inscrit ?
+                                    </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,7 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <i class="fa-solid fa-house"></i>
+      <a class="navbar-brand" href="{{ route('home')}}">
+        <i class="fa-solid fa-home"></i>
+        Acceuil
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -9,20 +10,30 @@
       <div class="collapse navbar-collapse" id="navbarColor01">
         <ul class="navbar-nav me-auto">
           <li class="nav-item">
-            <a class="nav-link" href="#">Home
-             
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link" href="{{ route('articles')}}">Articles</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">About</a>
-          </li>
-         
+          </li>  
+        </ul>
+
+        @if (Auth::user())
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+             <form action="{{ route('logout')}}" method="post">
+              @csrf
+              <button type="submit" class="btn primary">Déconnexion</button>
+            </form>
+            </li> 
+          </ul>
+        @else
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">Connexion</a>
+              </li> 
+            </ul> 
+        @endif
+        
       </div>
     </div>
   </nav>
